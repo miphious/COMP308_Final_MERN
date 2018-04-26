89a2a38d55b4f8dc01d73e20d5655baf81098e90
Suthas Ganeshathasan <suthas.gan@gmail.com>
2018-04-25 22:46:29 -0400

    updated

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,33 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import {BrowserRouter, Route, Link} from 'react-router-dom';
+import logo from './Assets/doctor.jpg';
 import './App.css';
+import Header  from './Components/Header';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
+      <BrowserRouter>
+      <div>
+        <Header/>
+        <br/>
+        <div className="w3-container">
+        <img src={logo} className="image" alt="doctor"/>
+        {/* <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to MyClinic</h1>
-        </header>
+        </header> */}
+
         <p className="App-intro">
           MyClinic is used in our various clinics across ontario, to help patients and nurses.
           Nurses can monitor patients during the first weeks of their release form the clinic.
           Patients can monitor ther own daily activites as well.
           To begin please Log In or Register if you are a new user. 
         </p>
+        </div>
+        </div>
+        </BrowserRouter>
       </div>
     );
   }
